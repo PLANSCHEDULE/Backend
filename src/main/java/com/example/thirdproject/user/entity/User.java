6cfdf38e8a90ffa096fc96f39bd269fe6d340a35
hyeonpark-5 <sorkgus@gmail.com>
2026-05-12 10:53:42 +0900

    feat: CustomUserDetails 구현

--- src/main/java/com/example/thirdproject/user/entity/User.java
+++ src/main/java/com/example/thirdproject/user/entity/User.java
@@ -1,9 +1,15 @@
 package com.example.thirdproject.user.entity;
 
+import com.example.thirdproject.user.Role;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,5 +20,9 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.ROLE_USER;
 
 }
