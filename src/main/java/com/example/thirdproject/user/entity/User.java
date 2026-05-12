dd722adf6c93c3be6c03c459f7b42e922bb58d09
hyeonpark-5 <sorkgus@gmail.com>
2026-05-12 09:44:08 +0900

    feat: user entity 작성

---
+++ src/main/java/com/example/thirdproject/user/entity/User.java
@@ -0,0 +1,18 @@
+package com.example.thirdproject.user.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
