d3557fc65db95df336f3cbd6b847f34ad83e47e0
hyeonpark-5 <sorkgus@gmail.com>
2026-05-12 12:28:46 +0900

    chore: SecurityConfig authenticationmanager 등록

--- src/main/java/com/example/thirdproject/global/config/SecurityConfig.java
+++ src/main/java/com/example/thirdproject/global/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import com.example.thirdproject.global.security.jwt.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +23,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config
+    ) throws Exception {
+        return config.getAuthenticationManager()
     }
 
     @Bean
