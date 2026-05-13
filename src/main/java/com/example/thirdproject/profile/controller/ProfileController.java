f77cff1660eb601545394ea79dc3a2449d2f03be
hyeonpark-5 <sorkgus@gmail.com>
2026-05-13 14:45:19 +0900

    feat: profile 생성 controller 구현

--- src/main/java/com/example/thirdproject/profile/controller/ProfileController.java
+++ src/main/java/com/example/thirdproject/profile/controller/ProfileController.java
@@ -4,16 +4,14 @@ import com.example.thirdproject.global.commonResponse.ApiResponse;
 import com.example.thirdproject.global.security.jwt.CustomUserDetails;
 import com.example.thirdproject.profile.dto.ProfileRequest;
 import com.example.thirdproject.profile.dto.ProfileResponse;
+import com.example.thirdproject.profile.dto.ProfileUpdate;
 import com.example.thirdproject.profile.service.ProfileService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/profiles")
@@ -33,6 +31,20 @@ public class ProfileController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 ApiResponse.created("프로필 생성 완료", response, request.getRequestURI())
+        );
+
+    }
+
+    @PatchMapping
+    public ResponseEntity<ApiResponse<ProfileResponse>> updateProfile(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestBody ProfileUpdate profileUpdate,
+            HttpServletRequest request
+            ) {
+        ProfileResponse response = profileService.updateProfile(userDetails.getUser().getId(), profileUpdate);
+
+        return ResponseEntity.ok(
+                ApiResponse.success("프로필 수정 완료", response, request.getRequestURI())
         );
 
     }
