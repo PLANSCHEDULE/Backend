bd3cdb56c652e87f5655ffc4173e93438acc4701
hyeonpark-5 <sorkgus@gmail.com>
2026-05-13 12:28:46 +0900

    feat: profile entity 구현

---
+++ src/main/java/com/example/thirdproject/profile/entity/Profile.java
@@ -0,0 +1,40 @@
+package com.example.thirdproject.profile.entity;
+
+import com.example.thirdproject.global.entity.BaseTime;
+import com.example.thirdproject.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "profiles")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Profile extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String handle;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder
+    public Profile(User user, String handle, String nickname, String bio) {
+        this.user = user;
+        this.handle = handle;
+        this.nickname = nickname;
+        this.bio = bio;
+    }
+}
