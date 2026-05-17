7ef1e0c4d09eeb03cc8ca00ea3423e73138fb67c
hyeonpark-5 <sorkgus@gmail.com>
2026-05-17 21:34:29 +0900

    feat: FavoriteController 좋아요 누른 전체 템플릿 조회

--- src/main/java/com/example/thirdproject/favorite/controller/FavoriteController.java
+++ src/main/java/com/example/thirdproject/favorite/controller/FavoriteController.java
@@ -3,8 +3,12 @@ package com.example.thirdproject.favorite.controller;
 import com.example.thirdproject.favorite.service.FavoriteService;
 import com.example.thirdproject.global.commonResponse.ApiResponse;
 import com.example.thirdproject.global.security.jwt.CustomUserDetails;
+import com.example.thirdproject.post.dto.PostTemplateResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -32,7 +36,18 @@ public class FavoriteController {
     }
 
 
-
-
     // 찜한 템플릿 전체 조회
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<Slice<PostTemplateResponse>>> getMyLikedPosts(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PageableDefault(size = 10)Pageable pageable,
+            HttpServletRequest request
+            ) {
+        Slice<PostTemplateResponse> responses =
+                favoriteService.getMyLikedTemplates(userDetails.getUser().getId(), pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                ApiResponse.success("내가 좋아요한 템플릿 전체 목록 조회 완료", responses, request.getRequestURI())
+        );
+    }
 }
