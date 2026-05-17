208af9f90f7891145bf242441fe22af039d86438
hyeonpark-5 <sorkgus@gmail.com>
2026-05-17 18:35:01 +0900

    feat: favorite 토글 api 구현

--- src/main/java/com/example/thirdproject/favorite/controller/FavoriteController.java
+++ src/main/java/com/example/thirdproject/favorite/controller/FavoriteController.java
@@ -1,35 +1,37 @@
 package com.example.thirdproject.favorite.controller;
 
+import com.example.thirdproject.favorite.service.FavoriteService;
 import com.example.thirdproject.global.commonResponse.ApiResponse;
+import com.example.thirdproject.global.security.jwt.CustomUserDetails;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/favorites")
+@RequiredArgsConstructor
 public class FavoriteController {
-    // 찜 추가
-    @PostMapping
-    public ResponseEntity<ApiResponse<Void>> FavoriteCreate(HttpServletRequest request) {
-        ApiResponse<Void> response = ApiResponse.created(
-                "",
-                null,
-                request.getRequestURI()
-        );
+    private final FavoriteService favoriteService;
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(response);
+    // 찜 토클
+    @PostMapping("/{postTemplateId}")
+    public ResponseEntity<ApiResponse<Void>> toggleFavorite(
+            @PathVariable Long postTemplateId,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            HttpServletRequest request) {
+       boolean isAdded = favoriteService.toggleFavorite(postTemplateId, userDetails.getUser().getId());
+
+       String message = isAdded ? "좋아요를 눌렀습니다." : "좋아요를 취소했습니다.";
+
+       return ResponseEntity.status(HttpStatus.OK).body(
+               ApiResponse.success(message, null, request.getRequestURI())
+       );
     }
 
 
-    // 찜 삭제
-    @DeleteMapping("/{favoriteId}")
-    public ResponseEntity<ApiResponse<Void>> updateTemplate(
-            HttpServletRequest request
-    ) {
-        return ResponseEntity.ok(ApiResponse.success("", null, request.getRequestURI()));
-    }
 
 
     // 찜한 템플릿 전체 조회
